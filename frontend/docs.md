84aeeb579513dace866fea16c5e507e52ea39092
FlDanyT <tdanyfl@gmail.com>
2026-01-14 11:04:06 +0300

    Frontend put запрос. Изменение категорий.

--- frontend/docs.md
+++ frontend/docs.md
@@ -52,6 +52,7 @@ function getCategoriesClick() {
       )  : (
         categories.map((cat) => (
           <div key={cat.id}>
+              <h1>{cat.id}</h1>
               <h1>{cat.name}</h1>
               <p>{cat.description}</p>
             </div>
@@ -80,4 +81,28 @@ function postCategoriesClick() {
 <input onChange={(e) => SetName(e.target.value)}/>
 <input onChange={(e) => SetDescription(e.target.value)}/>
 <button onClick={postCategoriesClick}>Добавить</button>
+```
+** PUT запрос к api. Изменение категорий **
+```javascript
+const [updateId, SetUpdateId] = useState("")
+const [updateName, SetUpdateName] = useState("")
+const [updateDescription, SetUpdateDescription] = useState("")
+
+function updateCategoriesClick() {
+
+    fetch('http://127.0.0.1:8000/categories/' + updateId + '/', {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json'
+      }, body: JSON.stringify({"name": updateName, "description": updateDescription})
+    })
+
+}
+```
+```html
+<h1>Изменить категорию</h1>
+<input onChange={(e) => SetUpdateId(e.target.value)}/>
+<input onChange={(e) => SetUpdateName(e.target.value)}/>
+<input onChange={(e) => SetUpdateDescription(e.target.value)}/>
+<button onClick={updateCategoriesClick}>Изменить</button>
 ```
--- frontend/shop/src/App.js
+++ frontend/shop/src/App.js
@@ -9,6 +9,10 @@ function App() {
   const [name, SetName] = useState("")
   const [description, SetDescription] = useState("")
 
+  const [updateId, SetUpdateId] = useState("")
+  const [updateName, SetUpdateName] = useState("")
+  const [updateDescription, SetUpdateDescription] = useState("")
+
   function getCategoriesClick() {
     fetch('http://127.0.0.1:8000/categories/')
     .then((response) => {
@@ -31,6 +35,17 @@ function App() {
 
   }
 
+  function updateCategoriesClick() {
+
+    fetch('http://127.0.0.1:8000/categories/' + updateId + '/', {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json'
+      }, body: JSON.stringify({"name": updateName, "description": updateDescription})
+    })
+
+  }
+
   return (
     <div className='App'>
       <h1>Посмотреть категории</h1>
@@ -40,6 +55,7 @@ function App() {
       )  : (
         categories.map((cat) => (
           <div key={cat.id}>
+              <h1>{cat.id}</h1>
               <h1>{cat.name}</h1>
               <p>{cat.description}</p>
             </div>
@@ -52,14 +68,15 @@ function App() {
       <button onClick={postCategoriesClick}>Добавить</button>
 
       <h1>Изменить категорию</h1>
-      <input/>
-      <input/>
-      <input/>
-      <button>Изменить</button>
+      <input onChange={(e) => SetUpdateId(e.target.value)}/>
+      <input onChange={(e) => SetUpdateName(e.target.value)}/>
+      <input onChange={(e) => SetUpdateDescription(e.target.value)}/>
+      <button onClick={updateCategoriesClick}>Изменить</button>
 
       <h1>Удалить категорию</h1>
       <input/>
       <button>Удалить</button>
+
     </div>
   );
 }
