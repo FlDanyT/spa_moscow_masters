e7d804a5645110864eae2877f0ee8075386fcc36
FlDanyT <tdanyfl@gmail.com>
2026-01-14 10:17:46 +0300

    Frontend вывод данных на страницу. Настройка api для работы ( в первом этапе не нужно знать )

--- frontend/docs.md
+++ frontend/docs.md
@@ -2,3 +2,58 @@
 ```bash
 npx create-react-app shop
 ```
+Далее удаляем все не нужное.
+** Работа с категориями **
+Делаем верстку
+```html
+    <div className='App'>
+      <h1>Посмотреть категории</h1>
+      <button>Посмотреть</button>
+
+      <h1>Добавить категорию</h1>
+      <input placeholder='name'/>
+      <input placeholder='description'/>
+      <button>Добавить</button>
+
+      <h1>Изменить категорию</h1>
+      <input placeholder='id'/>
+      <input placeholder='name'/>
+      <input placeholder='description'/>
+      <button>Изменить</button>
+
+      <h1>Удалить категорию</h1>
+      <input placeholder='id'/>
+      <button>Удалить</button>
+    </div>
+```
+Get запрос к api
+```javascript
+import { useState } from 'react';
+
+const [categories, setCotegories] = useState([])
+
+function getCategoriesClick() {
+    fetch('http://127.0.0.1:8000/categories/')
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      setCotegories(data)
+    })
+}
+```
+```html
+<button onClick={getCategoriesClick}>Посмотреть</button>
+```
+Выводим данные
+```html
+{ categories.length === 0 ? (
+        <p></p>
+      )  : (
+        categories.map((cat) => (
+          <div key={cat.id}>
+              <h1>{cat.name}</h1>
+              <p>{cat.description}</p>
+            </div>
+        ))
+)}
--- frontend/shop/src/App.js
+++ frontend/shop/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import logo from './logo.svg'; // src={logo} 
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [categories, setCotegories] = useState([])
+
+  function getCategoriesClick() {
+    fetch('http://127.0.0.1:8000/categories/')
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      setCotegories(data)
+    })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Посмотреть категории</h1>
+      <button  onClick={getCategoriesClick}>Посмотреть</button>
+      { categories.length === 0 ? (
+        <p></p>
+      )  : (
+        categories.map((cat) => (
+          <div key={cat.id}>
+              <h1>{cat.name}</h1>
+              <p>{cat.description}</p>
+            </div>
+        ))
+      )}
+
+      <h1>Добавить категорию</h1>
+      <input placeholder='name'/>
+      <input placeholder='description'/>
+      <button>Добавить</button>
+
+      <h1>Изменить категорию</h1>
+      <input placeholder='id'/>
+      <input placeholder='name'/>
+      <input placeholder='description'/>
+      <button>Изменить</button>
+
+      <h1>Удалить категорию</h1>
+      <input placeholder='id'/>
+      <button>Удалить</button>
     </div>
   );
 }
